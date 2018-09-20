da2d61c0f543dc75ec7cbf81be092470886b84cd
troy <gordontaaj@gmail.com>
2018-09-20 15:51:20 -0700

    making a repo of my side projects

---
+++ js-basics/index.js
@@ -0,0 +1,29 @@
+let name ='troy';
+console.log(name);
+// names cannot be a reserved keyword
+// names should be meaningful
+// names cannot start with a number
+// cannot contain a space or hyphen
+// names are case sensitive
+
+let fisrtName= 'Troy';
+let lastName = 'Gordon';
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+//constant cannot be reassigned
+// constant = const
+
+// primatives/value types = String Number Boolean Undefined Null
+//reference types
+
+
+let name = 'Troy'; // string Literal
+let age = 24; // number literal
+let isApproved = true; // Boolean literal = true or false
+let selectedColor = null; // clears the value of a variable
+
+
+
+
